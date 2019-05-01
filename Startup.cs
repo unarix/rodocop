1f9f27e9602d6b8ee0da99f84d8c32d1ae644232
ntello <team@unarix.com.ar>
2019-05-01 20:47:53 -0300

    MOD: default index

--- Startup.cs
+++ Startup.cs
@@ -6,33 +6,59 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace rodocop
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
             services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+          
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+
 
             // app.Run(async (context) =>
             // {
             //     await context.Response.WriteAsync("Hello World!");
             // });
-            app.UseStaticFiles();
+            app.UseHttpsRedirection();
+            app.UseCookiePolicy();
             app.UseDefaultFiles();
-            app.UseCors(builder => builder.WithOrigins("https://a2klabs.azurewebsites.net", "https://localhost:5001","https://localhost:5002","https://localhost:5003","https://localhost:5004"));
+            app.UseStaticFiles();
+            //app.UseCors(builder => builder.WithOrigins("https://a2klabs.azurewebsites.net", "https://localhost:5001","https://localhost:5002","https://localhost:5003","https://localhost:5004"));
         }
     }
 }
